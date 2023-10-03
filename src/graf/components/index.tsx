f1d74400ac2123ee878d657260763a4540872a1d
jigonzalez930209 <jigonzalez930209@gmail.com>
2023-10-03 16:09:14 -0300

    feat: Implement `app-bar`, `drawer` and plotly chart

--- src/graf/components/index.tsx
+++ src/graf/components/index.tsx
@@ -1,19 +1,19 @@
-import * as _ from 'lodash'
 import * as React from 'react'
-import { listen } from '@tauri-apps/api/event'
 import { Backdrop } from '@mui/material'
-import { useSnackbar } from 'notistack'
+import { listen } from '@tauri-apps/api/event'
+import * as _ from 'lodash'
 
-import { useData } from '../hooks/useData'
-import { LoadingsContext } from '../context/Loading'
+import AppBar from '@/components/app-bar'
+import Drawer from '@/components/drawer'
+import Loader from '@/components/loader'
+
 import { GrafContext } from '../context/GraftContext'
-import { readAllFiles, readAllFilesUsingWebProcess, readFilesUsingTauriProcess } from '../utils'
-
-import AppBar from './AppBar'
-import Loader from './Loader/Loader'
-import PlotlyChart from './GrafContainer'
-import DragDrop from './FileList/drag-drop/DragDrop'
+import { LoadingsContext } from '../context/Loading'
+import { useData } from '../hooks/useData'
 import { IPlatform } from '../interfaces/interfaces'
+import { readAllFiles, readFilesUsingTauriProcess } from '../utils'
+import DragDrop from './FileList/drag-drop/DragDrop'
+import PlotlyChart from './GrafContainer'
 
 const Index: React.FC = () => {
   const { updateData, data } = useData()
@@ -24,81 +24,47 @@ const Index: React.FC = () => {
   } = React.useContext(LoadingsContext)
   const [platform, setPlatform] = React.useState<IPlatform>(null)
 
-  const { enqueueSnackbar } = useSnackbar()
-
-  const handleFileUploadUsingWebProcess = async (files: FileList) => {
+  const readFiles = React.useCallback(async () => {
     setLoading(true)
-    const filesData = await readAllFilesUsingWebProcess(files)
-    if (filesData.contents.length) {
-      updateData(filesData.contents)
-    }
-    if (filesData.notSupported.length) {
-      enqueueSnackbar(`Not supported files: ${filesData.notSupported.join(', ')}`, { variant: 'error' })
-    }
-    setLoading(false)
-  }
 
-  const readFiles = async (fileList: FileList | undefined) => {
-    setLoading(true)
-    if (platform === 'web') {
-      handleFileUploadUsingWebProcess(fileList)
-    } else if (platform === 'desktop') {
-      updateData(await readFilesUsingTauriProcess().finally(() => setLoading(false)))
-    } else {
-      enqueueSnackbar(`Occur an error`, { variant: 'error' })
-    }
-    setLoading(false)
-  }
+    updateData(
+      await readFilesUsingTauriProcess().finally(() => setLoading(false))
+    )
+  }, [])
 
   const handleFileDropChange = React.useCallback(async () => {
-    if (!window.__TAURI_METADATA__) {
-      enqueueSnackbar('File drop is not supported for now', { variant: 'error' })
-      return
-    }
-    listen('tauri://file-drop', async event => {
+    listen('tauri://file-drop', async (event) => {
       const files = await readAllFiles(event.payload)
       if (files.contents.length) {
         setLoading(true)
         updateData(await files.contents)
         setLoading(false)
       }
-      if (files.notSupported.length)
-        enqueueSnackbar(`Not supported files: ${files.notSupported.join(', ')}`, { variant: 'error' })
+      if (files.notSupported.length) {
+      }
     })
   }, [loading])
 
   React.useEffect(() => {
     handleFileDropChange()
-    if (window.__TAURI_METADATA__) {
-      setPlatform('desktop')
-    } else {
-      setPlatform('web')
-    }
+
+    setPlatform('desktop')
     setLoading(false)
   }, [])
 
   return (
-    <div>
-      {loading && (
-        <Backdrop
-          sx={{ color: '#fff', zIndex: theme => theme.zIndex.drawer + 1 }}
-          open={loading}
-          onClick={() => {}}
-        >
-          <Loader type='hash' />
-        </Backdrop>
-      )}
-      <AppBar
-        readAllFiles={readFiles}
-        files={data}
-        platform={platform}
-        content={
-          <div>
-            {graftState?.fileType === 'csv' ? <DragDrop PlotlyChart={<PlotlyChart />} /> : <PlotlyChart />}
-          </div>
-        }
-      />
-    </div>
+    <>
+      <AppBar />
+      {loading && <Loader />}
+      <div className='flex h-full w-full'>
+        <Drawer variant='left' />
+        {graftState?.fileType === 'csv' ? (
+          <DragDrop PlotlyChart={<PlotlyChart />} />
+        ) : (
+          <PlotlyChart />
+        )}
+      </div>
+    </>
   )
 }
 
