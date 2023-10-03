b01764e337996c1a00da8eacf6cd75dbd89682af
jigonzalez930209 <jigonzalez930209@gmail.com>
2023-10-03 16:03:27 -0300

    feat: Create `menu` component

---
+++ src/components/menu/menu.tsx
@@ -0,0 +1,92 @@
+'use client'
+
+import * as React from 'react'
+import { getName, getVersion } from '@tauri-apps/plugin-app'
+import { AreaChartIcon } from 'lucide-react'
+import { WindowTitlebar } from 'tauri-controls'
+
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from '@/components/ui/menubar'
+
+import { AboutDialog } from '../about-dialog'
+import { Dialog, DialogTrigger } from '../ui/dialog'
+import CustomTooltip from '../ui/tooltip'
+import { MenuModeToggle } from './menu-mode-toggle'
+import { ProjectMenu } from './project'
+
+export function Menu() {
+  const [name, setName] = React.useState('')
+
+  const closeWindow = React.useCallback(async () => {
+    const { appWindow } = await import('@tauri-apps/plugin-window')
+
+    appWindow.close()
+  }, [])
+
+  React.useEffect(() => {
+    let name = ''
+    getName().then((x) =>
+      getVersion()
+        .then((y) => (name = `${x}_${y}`))
+        .finally(() => setName(name))
+    )
+  }, [window.__TAURI_METADATA__])
+
+  return (
+    <WindowTitlebar>
+      <Menubar className='z-50 w-full rounded-none border-b border-none pl-2 lg:pl-3'>
+        <MenubarMenu>
+          <CustomTooltip
+            title='Graf is a tool for creating and managing graphs.'
+            Icon={
+              <div className='inline-flex h-fit w-fit items-center text-cyan-500'>
+                <AreaChartIcon className='h-5 w-5' />
+              </div>
+            }
+          />
+        </MenubarMenu>
+
+        <MenubarMenu>
+          <MenubarTrigger className='mx-auto min-w-max font-bold capitalize'>
+            {name}
+          </MenubarTrigger>
+          <Dialog modal={false}>
+            <MenubarContent>
+              <DialogTrigger asChild>
+                <MenubarItem>About App</MenubarItem>
+              </DialogTrigger>
+
+              <MenubarSeparator />
+              <MenubarItem>
+                Preferences... <MenubarShortcut>⌘,</MenubarShortcut>
+              </MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>
+                Hide Music... <MenubarShortcut>⌘H</MenubarShortcut>
+              </MenubarItem>
+              <MenubarItem>
+                Hide Others... <MenubarShortcut>⇧⌘H</MenubarShortcut>
+              </MenubarItem>
+              <MenubarShortcut />
+              <MenubarItem onClick={closeWindow}>
+                Quit Music <MenubarShortcut>⌘Q</MenubarShortcut>
+              </MenubarItem>
+            </MenubarContent>
+            <AboutDialog />
+          </Dialog>
+        </MenubarMenu>
+        <ProjectMenu />
+
+        <MenuModeToggle />
+        <div className='w-full text-center'>{name}</div>
+      </Menubar>
+    </WindowTitlebar>
+  )
+}
---
+++ src/components/menu/project.tsx
@@ -0,0 +1,77 @@
+import * as React from 'react'
+import { GrafContext } from '@/graf/context/GraftContext'
+import { useData } from '@/graf/hooks/useData'
+import { readAllFiles, readFilesUsingTauriProcess } from '@/graf/utils'
+import { listen } from '@tauri-apps/api/event'
+
+import {
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from '../ui/menubar'
+import { useToast } from '../ui/use-toast'
+
+export const ProjectMenu = () => {
+  const { graftState, setFiles, setGraftState } = React.useContext(GrafContext)
+  const [loading, setLoading] = React.useState(false)
+  const t = useToast()
+  const { updateData } = useData()
+
+  const addNewProject = () => {}
+
+  const readFiles = React.useCallback(async () => {
+    setLoading(true)
+
+    updateData(
+      await readFilesUsingTauriProcess().finally(() => setLoading(false))
+    )
+  }, [])
+
+  const handleFileDropChange = React.useCallback(async () => {
+    listen('tauri://file-drop', async (event) => {
+      const files = await readAllFiles(event.payload)
+      if (files.contents.length) {
+        setLoading(true)
+        updateData(await files.contents)
+        setLoading(false)
+      }
+      if (files.notSupported.length)
+        t.toast({
+          title: 'Error Reading Files',
+          description: `Not supported files: ${files.notSupported.join(', ')}`,
+        })
+    })
+  }, [])
+
+  React.useEffect(() => {
+    handleFileDropChange()
+  }, [window.__TAURI_METADATA__])
+
+  return (
+    <MenubarMenu>
+      <MenubarTrigger className='relative'>Project</MenubarTrigger>
+      <MenubarContent>
+        {/* TODO: Implement handle project in next versions */}
+        <MenubarItem disabled onClick={addNewProject}>
+          New <MenubarShortcut>⌘N</MenubarShortcut>
+        </MenubarItem>
+        <MenubarItem onClick={readFiles}>
+          Open <MenubarShortcut>⌘O</MenubarShortcut>
+        </MenubarItem>
+        <MenubarItem>
+          Save <MenubarShortcut>⌘S</MenubarShortcut>
+        </MenubarItem>
+        <MenubarSeparator />
+        <MenubarItem>
+          Import <MenubarShortcut>⌘I</MenubarShortcut>
+        </MenubarItem>
+        <MenubarItem>
+          Export <MenubarShortcut>⌘E</MenubarShortcut>
+        </MenubarItem>
+      </MenubarContent>
+    </MenubarMenu>
+  )
+}
