31a8be53f7c14336e06977f8605d8fe09c6d921c
jigonzalez930209 <jigonzalez930209@gmail.com>
2023-10-03 15:55:25 -0300

    chore: Add configs and packages

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,18 +5,16 @@
 )]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
         .plugin(tauri_plugin_app::init())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_window::init())
+        .plugin(tauri_plugin_store::Builder::default().build())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_dialog::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
