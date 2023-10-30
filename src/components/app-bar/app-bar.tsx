ea11bf72b7718fec2f920bfd4ff68b4bec964603
José Iván González Jorge <jigonzalez930209@gmail.com>
2023-10-30 09:41:44 -0300

    feat: Change app bar to eliminate it

--- src/components/app-bar/app-bar.tsx
+++ src/components/app-bar/app-bar.tsx
@@ -10,6 +10,7 @@ import {
 
 import { GrafContext } from '../../graf/context/GraftContext'
 import ExportModal from '../export-dialog'
+import FrequencyAnalysisDialog from '../frequency-analysis/frequency-analysis-dialog'
 import ProjectHandler from './project-handler'
 import { Settings } from './settings'
 
@@ -18,40 +19,7 @@ const Bar: React.FC = ({}) => {
     graftState: { fileType },
   } = React.useContext(GrafContext)
 
-  return (
-    <div className='mt-2 flex gap-3 pl-4 '>
-      {/* <CustomPopover>
-        <PopoverTrigger>
-          <Button className='uppercase' variant='ghost'>
-            Project
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className=' bg-secondary'>
-          <Project />
-          <Popover.Arrow className='fill-primary' />
-        </PopoverContent>
-      </CustomPopover> */}
-      <CustomPopover>
-        <PopoverTrigger>
-          <Button className='uppercase' variant='ghost'>
-            settings
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className='h-auto w-auto bg-secondary'>
-          <Settings />
-          <Popover.Arrow className='fill-primary' />
-        </PopoverContent>
-      </CustomPopover>
-      <ProjectHandler />
-      {!['csv', null].includes(fileType) && (
-        <ExportModal>
-          <Button className='uppercase' variant='ghost'>
-            Export
-          </Button>
-        </ExportModal>
-      )}
-    </div>
-  )
+  return <div></div>
 }
 
 export default Bar
