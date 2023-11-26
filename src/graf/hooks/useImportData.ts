ca11470e3cba16cbdb462cb5d43a2d0982ea0553
José Iván González Jorge <jigonzalez930209@gmail.com>
2023-11-26 17:54:37 -0300

    feat: Create useImportData hook

---
+++ src/graf/hooks/useImportData.ts
@@ -0,0 +1,35 @@
+import * as React from 'react'
+
+import { GrafContext } from '../context/GraftContext'
+import { ProcessFile } from '../interfaces/interfaces'
+import { generateRandomId } from '../utils/common'
+
+type ImportData = {
+  name: string
+  content: ProcessFile['content']
+  color?: string
+  impParams?: ProcessFile['impedance']
+}
+
+const useImportData = () => {
+  const { setFile } = React.useContext(GrafContext)
+
+  const importDataTeq4Z = ({ name, content, color, impParams }: ImportData) => {
+    const file: ProcessFile = {
+      id: generateRandomId(),
+      name,
+      type: 'teq4Z',
+      content,
+      color: color || 'gray',
+      selected: false,
+      impedance: impParams,
+    }
+    setFile(file)
+  }
+
+  return { importDataTeq4Z }
+}
+
+// TODO: Add import multiples files
+
+export default useImportData
