dfc9c1a89504fc424febc76d4fa2b7262fc33ac4
José Iván González Jorge <jigonzalez930209@gmail.com>
2023-12-18 13:51:00 -0300

    feat: Change `DialogTitle` Component

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -43,10 +43,6 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className='absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity data-[state=open]:bg-accent data-[state=open]:text-muted-foreground hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none'>
-        <X className='h-4 w-4' />
-        <span className='sr-only'>Close</span>
-      </DialogPrimitive.Close>
     </DialogPrimitive.Content>
   </DialogPortal>
 ))
@@ -87,11 +83,17 @@ const DialogTitle = React.forwardRef<
   <DialogPrimitive.Title
     ref={ref}
     className={cn(
-      'text-lg font-semibold leading-none tracking-tight',
+      'flex justify-between rounded-md px-3 py-1 text-lg font-semibold leading-none tracking-tight',
       className
     )}
     {...props}
-  />
+  >
+    {props.children}
+    <DialogPrimitive.Close className=' rounded-sm opacity-70 ring-offset-background transition-opacity data-[state=open]:bg-accent data-[state=open]:text-muted-foreground hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none'>
+      <X className='h-4 w-4' />
+      <span className='sr-only'>Close</span>
+    </DialogPrimitive.Close>
+  </DialogPrimitive.Title>
 ))
 DialogTitle.displayName = DialogPrimitive.Title.displayName
 
